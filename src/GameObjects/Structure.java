ddb776533b33dc1c721da898a9b9219e66cb74ab
wangyijie6646 <yijiewang1337@gmail.com>
2018-04-25 12:13:31 -0400

    Copying local java file
    Copies all files stored locally.
    Files include
    -Player class
    -Structure class

---
+++ src/GameObjects/Structure.java
@@ -0,0 +1,12 @@
+package GameObjects;
+import GameControl.Player;
+
+public class Structure {
+	public int health;
+	public int armour;
+	public int damage;
+	public int width;
+	public int height;
+	public String name;
+	public Player player;
+}
